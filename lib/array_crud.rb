12490ca93b9e305a1e9eaf2732043b2fc432e4ce
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-03 21:14:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array= [1, 2, 3, 4, "totally!"]
+array [index_number] = element
 end
